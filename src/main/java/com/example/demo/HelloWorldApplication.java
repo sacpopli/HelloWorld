52a66ae89acb176966cad575cd6287f5ce2dfccc
SACHIN POPLI <sacpopli@in.ibm.com>
2020-11-24 16:57:17 +0530

    Update to /hello

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldApplication {
 
-  @RequestMapping("/")
+  @RequestMapping("/hello")
   public String home() {
     return "Hello Docker World";
   }
